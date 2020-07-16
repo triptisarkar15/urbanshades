fd26a68dd47502bbd0c8e13c4bc2cb18915ed5b3
Tripti Sarkar <triptisarkar15@gmail.com>
2020-07-16 09:07:58 +0530

    made css changes

--- src/components/Card/CardAbout.jsx
+++ src/components/Card/CardAbout.jsx
@@ -4,14 +4,17 @@ import { Card, Button } from 'react-bootstrap';
 
 const CardAbout = (props) => {
     return (
-        <div>
-        <Card style={{ width: '18rem' }}>
+        <div style={{marginTop:'-4292px', marginLeft:'15px', display:'flexbox'}}>
+        <Card style={{ paddingTop:'19px', boxSizing: 'border-box', paddingBottom:'16px'}}>
             <Card.Img variant="top" src={require('../../images/myself.png')}  />
             <Card.Body>
-                <Card.Title style={{color:'black'}}>About Me</Card.Title>
+                <Card.Title style={{color:'black' , textAlign:'center'}}>About Me</Card.Title>
                 <Card.Text style={{color:'black'}}>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
+                <strong><span>Hello Creatives !!</span></strong><br />
+                I am Tripti Sarkar. I am a Computer Science Engineer living in India.I am fond of sketching which acts as a mood-booster 
+                for me to feel refreshing and serene.
+                I admire to draw on locations whether indoors or outdoors (depending on my mood and weather conditions) capturing my sight. 
+                I feel contentment to share my creativity for drawing with everyone and make this process much more cheerful.
                 </Card.Text>
                
             </Card.Body>
