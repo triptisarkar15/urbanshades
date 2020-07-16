8ece6b374360efc5e130f3de3c9e0292a5abea6d
Tripti Sarkar <triptisarkar15@gmail.com>
2020-07-16 09:09:54 +0530

    Sidebar component

---
+++ src/containers/Sidebar/Sidebar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import './style.css';
+import { NavLink, Link } from 'react-router-dom';
+import { Card } from "react-bootstrap";
+
+const Sidebar = (props) => {
+
+    return (
+        <div style={{display:'flexbox',marginLeft:'15px'}} >
+            <Card style={{ marginTop: '-3496px', boxSizing: 'border-box' }}>
+                <Card.Body>
+                <Card.Title style={{color:'black' , textAlign:'center'}}><strong>Social Media Links</strong></Card.Title>
+                <Card.Text style={{color:'black'}}>Follow me on:</Card.Text>
+                <ul style={{color:'black'}}>
+                <li><a href='https://www.instagram.com' target='_blank' style={{color:'black'}}>Instagram</a></li>
+                <br />
+                <li><a href='https://www.facebook.com' target='_blank' style={{color:'black'}}>Facebook</a></li>
+                <br />
+                <li><a href='https://www.linkedin.com' target='_blank' style={{color:'black'}}>LinkedIn</a></li>
+                <br /> 
+                </ul> 
+                </Card.Body>
+            </Card>
+
+        </div>
+
+    );
+}
+
+export default Sidebar;
